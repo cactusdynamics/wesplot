2ad6360280a484625dcd24c31f88db94b7aa627d
Shuhao Wu <shuhao@shuhaowu.com>
2023-03-28 23:50:25 -0400

    Browser open in prod build

--- variant_prod.go
+++ variant_prod.go
@@ -4,11 +4,31 @@ package wesplot
 
 import (
 	"embed"
+	"os/exec"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
 )
 
 //go:embed webui
 var webuiFiles embed.FS
 
 func openBrowser(url string) {
+	var cmd string
+	var args []string
 
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	err := exec.Command(cmd, args...).Start()
+	if err != nil {
+		logrus.Warn("failed to start web browser automatically")
+	}
 }
